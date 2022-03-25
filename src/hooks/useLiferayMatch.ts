fdbcd1907dd2fb7e09798aa2fc31c8aaaf36ead8
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-03-26 07:17:02 +0900

    refactored to use friendly url mapping

--- src/hooks/useLiferayMatch.ts
+++ src/hooks/useLiferayMatch.ts
@@ -1,10 +1,19 @@
 import { useLocation, useMatch } from "react-router-dom";
-import { getBaseUrl } from "../utils";
+import { getBaseUrl, MissingPropertyError } from "../utils";
+import useLiferayContext from "./useLiferayContext";
 
 const useLiferayMatch = (path: string) => {
   const { pathname } = useLocation();
+  const { properties } = useLiferayContext();
+  const { friendlyUrlMapping } = properties;
+
+  if (!friendlyUrlMapping)
+    throw new MissingPropertyError(
+      "Property friendly-url-mapping not provided"
+    );
+
   const base = getBaseUrl(pathname);
-  return useMatch(`${base}${path}`);
+  return useMatch(`${base}${friendlyUrlMapping}${path}`);
 };
 
 export default useLiferayMatch;
--- src/hooks/useLiferayNavigate.ts
+++ src/hooks/useLiferayNavigate.ts
@@ -1,12 +1,21 @@
 import { NavigateOptions, useLocation, useNavigate } from "react-router-dom";
-import { getBaseUrl } from "../utils";
+import { getBaseUrl, MissingPropertyError } from "../utils";
+import useLiferayContext from "./useLiferayContext";
 
 const useLiferayNavigate = () => {
   const { pathname } = useLocation();
+  const { properties } = useLiferayContext();
+  const { friendlyUrlMapping } = properties;
+
+  if (!friendlyUrlMapping)
+    throw new MissingPropertyError(
+      "Property friendly-url-mapping not provided"
+    );
+
   const navigate = useNavigate();
   return (path: string, options?: NavigateOptions) => {
     const base = getBaseUrl(pathname);
-    navigate(`${base}${path}`, options);
+    navigate(`${base}${friendlyUrlMapping}${path}`, options);
   };
 };
 
